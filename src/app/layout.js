e9201240f1af2711cb073956c5be55e274c25976
PanCat26 <catalin.pangaleanu@gmail.com>
2025-05-21 13:18:25 +0300

    Added header.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,21 @@
 import "./globals.css";
+import { IBM_Plex_Mono } from 'next/font/google';
+
+const ibmMonoSemiBold = IBM_Plex_Mono({
+  weight: ['600'],
+  subsets: ['latin'],
+  variable: '--font-ibm-mono-semi-bold',
+});
 
 export const metadata = {};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={ibmMonoSemiBold.variable}>
+        <header>
+          <button id="logo">TradeLab</button>
+        </header>
         {children}
       </body>
     </html>
