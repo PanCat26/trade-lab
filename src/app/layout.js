1ca5fe3692ca17634e6039fcf265b79de6cf13af
PanCat26 <catalin.pangaleanu@gmail.com>
2025-05-21 13:45:53 +0300

    Added partial sidebar with home button.

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,14 @@ export default function RootLayout({ children }) {
         <header>
           <button id="logo">TradeLab</button>
         </header>
-        {children}
+        <main>
+          <nav id="sidebar">
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" height="68px" viewBox="0 0 24 24" width="68px" fill="#e6e6e6"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>
+            </button>
+          </nav>
+          {children}
+        </main>
       </body>
     </html>
   );
